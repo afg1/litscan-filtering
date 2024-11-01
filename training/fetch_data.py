a1c7ef87f9331ba8e6267c9c56b62e025ff38db1
Andrew Green <agreen@ebi.ac.uk>
2024-11-01 11:23:09 +0000

    Add data fetching from RNAc database

---
+++ training/fetch_data.py
@@ -0,0 +1,27 @@
+import polars as pl
+import os
+
+connstr = os.getenv("PGDATABASE")
+
+query = """
+select lsa.pmcid, max(title) as title, max(abstract) as abstract, array_agg(job_id) from litscan_article lsa
+join litscan_result lsr on lsr.pmcid = lsa.pmcid
+
+group by lsa.pmcid
+"""
+
+sentence_query = """
+select lsr.pmcid, array_agg(sentence) as sentence from litscan_result lsr
+join litscan_abstract_sentence lsas on lsas.result_id = lsr.id
+group by lsr.pmcid
+union 
+select lsr.pmcid, array_agg(sentence) as sentence from litscan_result lsr
+join litscan_body_sentence lsbs on lsbs.result_id = lsr.id
+where location != 'other'
+group by lsr.pmcid"""
+
+df = pl.read_database_uri(query, connstr)
+df.write_parquet("litscan_all_hit_articles.parquet")
+
+# sentences = pl.read_database_uri(sentence_query, connstr)
+# sentences.write_parquet("litscan_all_hit_sentences.parquet")
